68ed42e8f0e40fbae9981f1b83d58a7a600631e0
Jeroimo <jeronimoriveros@gmail.com>
2024-10-06 23:52:42 -0500

    midleware, pendiente resoler errores

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,3 @@
-/* midewalres for is teacher and is admin */
 const { User } = require('../model/user.model');
 
 const isTeacher = async (req, res, next) => {
@@ -15,15 +14,22 @@ const isTeacher = async (req, res, next) => {
 };
 
 const isAdmin = async (req, res, next) => {
+  console.log(req.body);
   try {
-    const user = await User.findOne({ where: { id: req.user.email } });
+    const user = await User.findOne({ where:  req.body.email });
+    
+    // Verificar si el usuario fue encontrado
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado.' });
+    }
+    
     if (user.role === 'admin') {
       next();
     } else {
       return res.status(403).json({ message: 'No tienes permisos para acceder a esta ruta.' });
     }
   } catch (error) {
-    return res.status(500).json({ message: 'Error al verificar permisos.' });
+    return res.status(500).json({ message: 'Error al verificar permisos.', error });
   }
 };
 
